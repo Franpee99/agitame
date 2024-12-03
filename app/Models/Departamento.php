4040d9ee0ca126e6bb03dcbb6f6797a4947c1b32
fran <fran@vbox.myguest.virtualbox.org>
2024-12-03 11:38:56 +0100

    Articulo y Factura creado, relacion entre articulo-departamento, articulo-factura y usuario-factura

--- app/Models/Departamento.php
+++ app/Models/Departamento.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Departamento extends Model
 {
@@ -15,5 +16,9 @@ class Departamento extends Model
     public function empleados()
     {
         return $this->hasMany(Empleado::class);
+    }
+
+    public function articulos(){
+        return $this->BelongsToMany(Articulo::class);
     }
 }
